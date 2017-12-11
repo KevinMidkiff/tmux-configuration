c52671976c155d623f589972663a43923efb62a9
Kevin Midkiff <midkiff.km@gmail.com>
2017-12-10 16:15:19 -0800

    Add 256 colour printing helper script

---
+++ print_256_colours.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Source: https://askubuntu.com/questions/821157/print-a-256-color-test-pattern-in-the-terminal
+
+##
+## Simple script to print 256 colours for helping pick a tmux colour 
+##
+
+for i in {0..255} ; do
+    printf "\x1b[48;5;%sm%3d\e[0m " "$i" "$i"
+    if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
+        printf "\n";
+    fi
+done
+
